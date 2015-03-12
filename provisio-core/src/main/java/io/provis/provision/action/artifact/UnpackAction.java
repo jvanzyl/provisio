f777661df7b1402ee4f299321da735e554995bff
Jason van Zyl <jason@tesla.io>
2015-03-12 16:43:56 -0700

    Allow the selective filtering of resources in the unpacked archive

--- provisio-core/src/main/java/io/provis/provision/action/artifact/UnpackAction.java
+++ provisio-core/src/main/java/io/provis/provision/action/artifact/UnpackAction.java
@@ -3,6 +3,7 @@ package io.provis.provision.action.artifact;
 import io.provis.model.ProvisioningAction;
 import io.provis.model.ProvisioningContext;
 import io.provis.model.io.InterpolatingInputStream;
+import io.tesla.proviso.archive.Selector;
 import io.tesla.proviso.archive.UnArchiver;
 import io.tesla.proviso.archive.UnarchivingEntryProcessor;
 
@@ -10,6 +11,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
 import java.util.Map;
 
 import javax.inject.Named;
@@ -34,12 +36,13 @@ public class UnpackAction implements ProvisioningAction {
   private boolean useRoot;
   private boolean flatten;
   private boolean filter;
-  //
+  private String filterIncludes;
   private Artifact artifact;
   private File outputDirectory;
 
   private static final Splitter splitter = Splitter.on(',').trimResults().omitEmptyStrings();
-  
+
+  @Override
   public void execute(ProvisioningContext context) {
 
     if (!outputDirectory.exists()) {
@@ -58,7 +61,11 @@ public class UnpackAction implements ProvisioningAction {
           .build();
 
       if (filter) {
-        unarchiver.unarchive(archive, outputDirectory, new FilteringProcessor(context.getVariables()));
+        if (filterIncludes != null) {
+          unarchiver.unarchive(archive, outputDirectory, new SelectiveFilteringProcessor(splitter.splitToList(includes), null, context.getVariables()));
+        } else {
+          unarchiver.unarchive(archive, outputDirectory, new FilteringProcessor(context.getVariables()));
+        }
       } else {
         unarchiver.unarchive(archive, outputDirectory);
       }
@@ -68,12 +75,12 @@ public class UnpackAction implements ProvisioningAction {
   }
 
   private String[] split(String s) {
-    if(s == null) {
+    if (s == null) {
       return new String[0];
     }
     return StringUtils.split(s, ",");
   }
-  
+
   public String getIncludes() {
     return includes;
   }
@@ -130,8 +137,34 @@ public class UnpackAction implements ProvisioningAction {
     this.filter = filter;
   }
 
+  class SelectiveFilteringProcessor implements UnarchivingEntryProcessor {
+
+    Selector selector;
+    Map<String, String> variables;
+
+    SelectiveFilteringProcessor(List<String> includes, List<String> excludes, Map<String, String> variables) {
+      this.variables = variables;
+      this.selector = new Selector(includes, excludes);
+    }
+
+    @Override
+    public String processName(String name) {
+      return name;
+    }
+
+    @Override
+    public void processStream(String entryName, InputStream inputStream, OutputStream outputStream) throws IOException {
+      if (selector.include(entryName)) {
+        ByteStreams.copy(new InterpolatingInputStream(inputStream, variables), outputStream);
+      } else {
+        ByteStreams.copy(inputStream, outputStream);
+      }
+    }
+  }
+
   class FilteringProcessor implements UnarchivingEntryProcessor {
 
+    Selector selector;
     Map<String, String> variables;
 
     FilteringProcessor(Map<String, String> variables) {
@@ -144,7 +177,7 @@ public class UnpackAction implements ProvisioningAction {
     }
 
     @Override
-    public void processStream(InputStream inputStream, OutputStream outputStream) throws IOException {
+    public void processStream(String entryName, InputStream inputStream, OutputStream outputStream) throws IOException {
       ByteStreams.copy(new InterpolatingInputStream(inputStream, variables), outputStream);
     }
   }
