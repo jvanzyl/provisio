de46402a21f1153ecd70efae26c650cf0386edc2
Jason van Zyl <jason@tesla.io>
2016-01-17 09:48:05 -0800

    Update license header

--- provisio-testing/src/main/java/io/provis/testing/ConsoleDependencyGraphDumper.java
+++ provisio-testing/src/main/java/io/provis/testing/ConsoleDependencyGraphDumper.java
@@ -1,13 +1,10 @@
-/*******************************************************************************
- * Copyright (c) 2010, 2011 Sonatype, Inc.
+/**
+ * Copyright (c) 2016 Takari, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    Sonatype, Inc. - initial API and implementation
- *******************************************************************************/
+ */
 package io.provis.testing;
 
 import java.io.PrintStream;
