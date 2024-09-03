04076fe9ec7e8189863569f9514fd286da695370
Tamas Cservenak <tamas@cservenak.net>
2024-09-03 17:55:29 +0200

    Simplify

--- provisio-core/src/main/java/ca/vanzyl/provisio/action/artifact/WriteToDiskAction.java
+++ provisio-core/src/main/java/ca/vanzyl/provisio/action/artifact/WriteToDiskAction.java
@@ -23,17 +23,20 @@ import ca.vanzyl.provisio.ProvisioningException;
 import ca.vanzyl.provisio.model.ProvisioArtifact;
 import ca.vanzyl.provisio.model.ProvisioningAction;
 import ca.vanzyl.provisio.model.ProvisioningContext;
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 import javax.inject.Named;
+import org.codehaus.plexus.util.io.CachingOutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Named("write")
 public class WriteToDiskAction implements ProvisioningAction {
-    private static Logger logger = LoggerFactory.getLogger(WriteToDiskAction.class);
+    private static final Logger logger = LoggerFactory.getLogger(WriteToDiskAction.class);
+    private static final int BUFFER_SIZE = 32 * 1024;
 
     private final ProvisioArtifact artifact;
     private final File outputDirectory;
@@ -58,18 +61,22 @@ public class WriteToDiskAction implements ProvisioningAction {
         File target = new File(outputDirectory, targetPath);
         try {
             Files.createDirectories(target.getParentFile().toPath());
-            if (ProvisioVariables.allowTargetOverwrite(context)) {
-                if (Files.isRegularFile(target.toPath())) {
-                    logger.warn("Conflict: artifact {} overwrote existing file {}", artifact, targetPath);
-                }
-                Files.copy(artifact.getFile().toPath(), target.toPath(), StandardCopyOption.REPLACE_EXISTING);
-            } else {
-                if (Files.isRegularFile(target.toPath())) {
+            if (Files.exists(target.toPath())) {
+                if (ProvisioVariables.allowTargetOverwrite(context)) {
+                    logger.warn("Conflict: artifact {} overwrites existing file {}", artifact, targetPath);
+                } else {
                     throw new ProvisioningException(
                             "Conflict: artifact " + artifact + " would overwrite existing file: " + targetPath);
                 }
-                logger.debug("Copying {} -> {}", artifact, targetPath);
-                Files.copy(artifact.getFile().toPath(), target.toPath());
+            }
+            try (CachingOutputStream outputStream = new CachingOutputStream(target.toPath(), BUFFER_SIZE);
+                    BufferedInputStream inputStream =
+                            new BufferedInputStream(new FileInputStream(artifact.getFile()), BUFFER_SIZE)) {
+                inputStream.transferTo(outputStream);
+                outputStream.close();
+                if (outputStream.isModified()) {
+                    target.setLastModified(artifact.getFile().lastModified());
+                }
             }
         } catch (IOException e) {
             throw new ProvisioningException("Error copying " + source + " to " + targetPath, e);
